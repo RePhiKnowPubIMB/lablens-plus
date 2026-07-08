9283a2dd84f4822e038be3c56d7c9edb80057f19
MD.ABDULLAH AL SAIM <2022331007@student.sust.edu>
2026-07-08 22:42:01 +0600

    Update CORS origin to production URL

--- server/src/index.js
+++ server/src/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:"lablens-plus-de7i.vercel.app",
     credentials: true,
   })
 );
